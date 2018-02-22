4057e98f373b09290984a113f4ef02268b789589
Sunil Ayyappan <sunilayyaps@gmail.com>
2018-02-21 21:41:58 -0500

    Add files via upload

---
+++ capstone-project2/ipl/data/credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = '3MHU9bQ2f3MletwPBwyOWPLZa'
+CONSUMER_SECRET = 'bWq5d0bcHfBhvspkM4jNLixWZ7Ur3F8FsCfFG2DpmF3ZHeUenl'
+
+# Access:
+ACCESS_TOKEN  = '1170850928-wARcpWKrrT3Xd6pAxwEg48aJzupTAbqNXJM7GGG'
+ACCESS_SECRET = '3csGEwYbxpMXliVqX3t7cqGITEbsrsJZp9nfOtSAMHJbt'
